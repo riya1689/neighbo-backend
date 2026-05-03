a4ce611a9ae1a571c1bde331ea03b39f49ca9a6f
riya1689 <riyaratri24@gmail.com>
2026-05-03 08:45:00 +0600

    chore: configure middleware not found error

---
+++ src/middleware/notFound.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express"; // TS Change: Imported Express types
+
+// TS Change: Added types for req and res
+function notFound(req: Request, res: Response): Response {
+  return res.status(404).json({ error: "Not Found" });
+}
+
+export default notFound;
