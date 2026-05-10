d9439c9c4631fedcf02bb0229fa68dff01c9df05
riya1689 <riyaratri24@gmail.com>
2026-05-10 11:36:25 +0600

    feat: update dashboard route

--- src/modules/dashboard/dashboard.routes.ts
+++ src/modules/dashboard/dashboard.routes.ts
@@ -1,5 +1,11 @@
 import express, { type Router } from "express";
-import { getStats, getTransactions, getPosts } from "./dashboard.controller.js";
+import { 
+  getStats, 
+  getTransactions, 
+  getPosts, 
+  getPurchases, 
+  getPremiumPlans 
+} from "./dashboard.controller.js";
 import { protect } from "../../middlewares/authMiddleware.js";
 
 const router: Router = express.Router();
@@ -7,5 +13,7 @@ const router: Router = express.Router();
 router.get("/stats", protect, getStats);
 router.get("/transactions", protect, getTransactions);
 router.get("/posts", protect, getPosts);
+router.get("/purchases", protect, getPurchases);
+router.get("/plans", protect, getPremiumPlans);
 
 export default router;
